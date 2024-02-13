817b161301d599ff1db4172d3899a33345a6db0f
Mário Coxe <mario.coxe@ideiasdinamicas.com>
2024-02-14 00:14:44 +0100

    feat: add calender Reducer

--- src/features/store/index.js
+++ src/features/store/index.js
@@ -1,13 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../authentication/authSlice"
 import studentReducer from "../student/studentSlice";
-import {findEventsByTeamReducer, findEventsBySearchReducer} from "../event/eventSlice";
-
+import { findEventsByTeamReducer, findEventsBySearchReducer } from "../event/eventSlice";
+import calenderReducer from "../calender/calenderSlice";
 export const Store = configureStore({
   reducer: {
     auth: authReducer,
     student: studentReducer,
     events: findEventsByTeamReducer,
-    eventsSearch: findEventsBySearchReducer
+    eventsSearch: findEventsBySearchReducer,
+    calender: calenderReducer
   },
 });
