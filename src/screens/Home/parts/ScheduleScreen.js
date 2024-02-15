828e6779ed563c6f4bbd5c09031eb0ad3e06d0e4
Mário Coxe <mario.coxe@ideiasdinamicas.com>
2024-02-15 11:37:58 +0100

    feat: update SheduleScreen

--- src/screens/Home/parts/ScheduleScreen.js
+++ src/screens/Home/parts/ScheduleScreen.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import { View, Text, FlatList } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, FlatList, TouchableOpacity, Modal, Image } from 'react-native';
 import styles from './styles/ScheduleScreenStyle';
 
 const ScheduleScreen = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedProfessor, setSelectedProfessor] = useState(null);
+
   const scheduleData = [
-    { id: '1', day: 'Segunda', time: '08:00 09:30', subject: 'MT' },
-    { id: '2', day: 'Terça', time: '10:00 11:30', subject: 'HIS' },
-    { id: '3', day: 'Quarta', time: '12:00 13:30', subject: 'ING' },
-    { id: '4', day: 'Quinta', time: '14:00 15:30', subject: 'PT' },
-    { id: '5', day: 'Sexta', time: '09:00 10:30', subject: 'GD' },
+    { id: '1', day: 'Segunda', time: '08:00 09:30', subject: 'MT', professor: { name: 'Evandro Eusebio', subject: 'Matemática', isDirector: true, photo: require('../../../../assets/image/users/evandro.jpeg') } },
+    { id: '2', day: 'Terça', time: '10:00 11:30', subject: 'HIS', professor: { name: 'José Luis', subject: 'História', isDirector: false, photo: require('../../../../assets/image/users/evandro.jpeg') } },
+    { id: '3', day: 'Quarta', time: '12:00 13:30', subject: 'ING', professor: { name: 'Mauro Coxe', subject: 'Inglês', isDirector: false, photo: require('../../../../assets/image/users/evandro.jpeg') } },
+    { id: '4', day: 'Quinta', time: '14:00 15:30', subject: 'PT', professor: { name: 'Ronaldo Coxe', subject: 'Português', isDirector: false, photo: require('../../../../assets/image/users/evandro.jpeg') } },
+    { id: '5', day: 'Sexta', time: '09:00 10:30', subject: 'GD', professor: { name: 'Isaías Coxe', subject: 'Geografia', isDirector: true, photo: require('../../../../assets/image/users/evandro.jpeg') } },
   ];
 
   const weekdays = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta'];
+
+  const openModal = (professor) => {
+    setSelectedProfessor(professor);
+    setModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setModalVisible(false);
+  };
 
   return (
     <View style={styles.container}>
@@ -30,21 +42,44 @@ const ScheduleScreen = () => {
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
           <View style={styles.row}>
-            <View style={styles.timeCell}>
+            <TouchableOpacity style={styles.timeCell}>
               <Text>{item.time}</Text>
-            </View>
+            </TouchableOpacity>
             {weekdays.map((day) => (
-              <View key={day} style={styles.cell}>
+              <TouchableOpacity
+                key={day}
+                style={styles.cell}
+                onPress={() => openModal(item.professor)}
+              >
                 {item.day === day && (
-                  <>
-                    <Text style={styles.subject}>{item.subject}</Text>
-                  </>
+                  <Text style={styles.subject}>{item.subject}</Text>
                 )}
-              </View>
+              </TouchableOpacity>
             ))}
           </View>
         )}
       />
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={closeModal}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalHeaderText}>Informações do Professor</Text>
+            <Image source={selectedProfessor?.photo} style={styles.professorImage} />
+            <Text style={styles.professorName}>{selectedProfessor?.name}</Text>
+            <View style={styles.professorDetails}>
+              {console.log(selectedProfessor?.name)}
+              <Text style={styles.professorSubject}>Disciplina: {selectedProfessor?.subject}</Text>
+            </View>
+          </View>
+          <TouchableOpacity style={styles.closeButton} onPress={closeModal}>
+            <Text style={styles.closeButtonText}>Fechar</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
     </View>
   );
 };
--- src/screens/Home/parts/styles/ScheduleScreenStyle.js
+++ src/screens/Home/parts/styles/ScheduleScreenStyle.js
@@ -1,6 +1,6 @@
 import { StyleSheet, Dimensions } from "react-native";
 
-const { width } = Dimensions.get('window');
+const { width, height } = Dimensions.get('window');
 
 export default StyleSheet.create({
   container: {
@@ -55,5 +55,89 @@ export default StyleSheet.create({
   subject: {
     fontSize: 16,
     color: '#333',
+  },
+
+
+
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.0)", 
+   
+  },
+  modalContent: {
+    backgroundColor: "#fff",
+    width: width * 0.8,
+    borderRadius: 20,
+    padding: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 10,
+    shadowRadius: 20,
+    elevation: 1000,
+    height: 220
+  },
+  modalHeaderText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
+    color: "#0077B6", // Cor de destaque para o cabeçalho
+    textAlign: "center",
+  },
+  modalText: {
+    fontSize: 16,
+    marginBottom: 10,
+    color: "#333", // Cor do texto principal
+    textAlign: "center",
+  },
+  closeButton: {
+    backgroundColor: "#0077B6", // Cor do botão de fechar
+    borderRadius: 10,
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    marginTop: 20,
+    alignSelf: "center",
+  },
+  closeButtonText: {
+    fontSize: 18,
+    color: "#fff", // Cor do texto do botão de fechar
+    fontWeight: "bold",
+  },
+  professorInfo: {
+    marginTop: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  professorImage: {
+    width: 110, // Largura da imagem do professor
+    height: 150, // Altura da imagem do professor
+    borderRadius: 30, // Bordas arredondadas para uma aparência mais suave
+    marginBottom: 10, // Espaço abaixo da imagem
+  },
+  professorDetails: {
+    alignContent: "flex-end",
+    alignItems: "flex-end"
+  },
+  professorName: {
+    fontSize: 18,
+    textAlign: 'center',
+    marginLeft: 135,
+    marginTop: -120
+  },
+  professorSubject: {
+    fontSize: 16,
+    marginBottom: 5,
+    color: "#555",
+    textAlign: "center", // Alinhamento centralizado do texto
+  },
+  professorRole: {
+    fontSize: 16,
+    color: "#555",
+    textAlign: "center", // Alinhamento centralizado do texto
   },
 });
